4b0f1093e8077eb5123e468109fbe46f321e532d
Julian Bernal <Julian-1705@hotmail.com>
2022-09-05 12:24:35 -0500

    the file recap.ts is created with basic concept to typecript and image is added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -5,7 +5,7 @@ import { HEROES } from '../mock-heroes'
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']  
+  styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
   // Propiedad del componente hero de tipo Hero:
@@ -17,6 +17,7 @@ export class HeroesComponent implements OnInit {
 
   // Propiedad del componente llamada heroes para exponer la matriz
   heroes = HEROES;
+  img = 'https://cdn-3.expansion.mx/dims4/default/a017412/2147483647/strip/true/crop/362x217+0+0/resize/1200x719!/format/webp/quality/90/?url=https%3A%2F%2Fcherry-brightspot.s3.amazonaws.com%2Fmedia%2F2015%2F04%2F29%2Fimageedit94629467394.jpg';
 
   constructor() { }
 
