f9ce9c14ab71d12b8e03652dd4d49526cd5aa9dc
Julian Bernal <Julian-1705@hotmail.com>
2022-09-06 10:20:21 -0500

    it is added a details section

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -14,7 +14,7 @@ export class HeroesComponent implements OnInit {
   public img: string = 'https://cdn-3.expansion.mx/dims4/default/a017412/2147483647/strip/true/crop/362x217+0+0/resize/1200x719!/format/webp/quality/90/?url=https%3A%2F%2Fcherry-brightspot.s3.amazonaws.com%2Fmedia%2F2015%2F04%2F29%2Fimageedit94629467394.jpg';
 
   // Controlador de eventos de clic (Se asigna una propiedad de componente)
-  selectedHero?: Hero;
+  public selectedHero?: Hero;
 
   constructor() { }
 
