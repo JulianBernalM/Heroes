3c38a0e9ae737048b5abf7d680382365f307c3b2
Julian Bernal <Julian-1705@hotmail.com>
2022-09-13 11:04:05 -0500

    The heroesComponente with heroService is update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,8 +9,8 @@ import { HEROES } from '../mock-heroes'
 })
 
 export class HeroesComponent implements OnInit {
-  // Propiedad del componente llamada heroes para exponer la matriz
-  public heroes = HEROES;
+
+  public heroes: Hero[] = [];
   public img: string = 'https://cdn-3.expansion.mx/dims4/default/a017412/2147483647/strip/true/crop/362x217+0+0/resize/1200x719!/format/webp/quality/90/?url=https%3A%2F%2Fcherry-brightspot.s3.amazonaws.com%2Fmedia%2F2015%2F04%2F29%2Fimageedit94629467394.jpg';
 
   // Click event controller (A component property is assigned)
